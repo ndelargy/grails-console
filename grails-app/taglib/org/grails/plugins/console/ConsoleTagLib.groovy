0116eeaf28ca1c25d6549386d9c71f97ff961ec9
Matt Sheehan <matt.sheehan@objectpartners.com>
2013-08-09 21:08:10 -0500

    update codemirror.

--- grails-app/taglib/org/grails/plugins/console/ConsoleTagLib.groovy
+++ grails-app/taglib/org/grails/plugins/console/ConsoleTagLib.groovy
@@ -17,7 +17,7 @@ class ConsoleTagLib {
 			out << r.layoutResources()
 		}
 		else {
-			out << """<link rel='stylesheet' media="screen" href='${resource(dir: 'js/CodeMirror-2.23/lib', file: 'codemirror.css', plugin: 'console')}' />"""
+			out << """<link rel='stylesheet' media="screen" href='${resource(dir: 'js/CodeMirror-3.15/lib', file: 'codemirror.css', plugin: 'console')}' />"""
 			out << """<link rel='stylesheet' media="screen" href='${resource(dir: 'css', file: 'jquery.layout.css', plugin: 'console')}' />"""
 			out << """<link rel='stylesheet' media="screen" href='${resource(dir: 'css', file: 'grails-console.css', plugin: 'console')}' />"""
 		}
@@ -32,7 +32,7 @@ class ConsoleTagLib {
 		else {
 			for (name in ['jquery-1.7.1.min', 'jquery-ui-1.8.17.custom.min',
 			              'jquery.layout', 'jquery.Storage', 'jquery.hotkeys',
-			              'CodeMirror-2.23/lib/codemirror', 'CodeMirror-2.23/mode/groovy/groovy',
+			              'codemirror-3.15/lib/codemirror', 'codemirror-3.15/mode/groovy/groovy',
 			              'grails-console/console']) {
 				out << g.javascript(src: name + '.js', plugin: 'console')
 			}
